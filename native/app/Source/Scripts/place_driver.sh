054c9ac668210ff8ef6b8c043ca08837d3abf1ea
nodeful <romankisilo@gmail.com>
2020-09-20 14:43:21 +0300

    changes

--- native/app/Source/Scripts/place_driver.sh
+++ native/app/Source/Scripts/place_driver.sh
@@ -5,5 +5,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Copy driver into Plug-Ins folder
 cp -f -r "$DIR/Embedded/eqMac.driver" /Library/Audio/Plug-Ins/HAL/ &>/dev/null || \
-  cp -f -r "../../Embedded/eqMac.driver" /Library/Audio/Plug-Ins/HAL/ # if running from terminal
+cp -f -r "$DIR/eqMac.driver" /Library/Audio/Plug-Ins/HAL/ &>/dev/null || \
+cp -f -r "../../Embedded/eqMac.driver" /Library/Audio/Plug-Ins/HAL/
 
