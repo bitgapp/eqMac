decf2673b1823d992af8b47b8acaf4ce3da953e8
Nodeful <romankisilo@gmail.com>
2021-03-24 13:25:52 +0200

    removed site from OS repo

--- site/src/app/services/constants.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core'
-
-@Injectable({
-  providedIn: 'root'
-})
-export class ConstantsService {
-  readonly GH_GROUP = 'bitgapp'
-  readonly GH_NAME = 'eqmac'
-}
--- site/src/app/site.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core'
-
-@Component({
-  selector: 'eqm-site',
-  template: `<router-outlet></router-outlet>`
-})
-export class SiteComponent {}
