c7cd637cc81daef4ba95242607884a42bff73fe3
Nodeful <romankisilo@gmail.com>
2021-05-04 02:58:35 +0300

    referencing components modules locally instead of building it

--- modules/components/dist/components/components/tooltip/tooltip.service.d.ts
+++
@@ -1,7 +0,0 @@
-import * as ɵngcc0 from '@angular/core';
-export declare class TooltipService {
-    components: any[];
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<TooltipService, never>;
-}
-
-//# sourceMappingURL=tooltip.service.d.ts.map
